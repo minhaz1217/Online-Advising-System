498e49ce1164b1297405f2dae7089d37428a87a1
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2018-11-27 00:30:10 +0600

    successfully getting student's take available courses from /student/available/{student_id}

--- src/main/java/io/github/minhaz1217/onlineadvising/Controller/StudentController.java
+++ src/main/java/io/github/minhaz1217/onlineadvising/Controller/StudentController.java
@@ -9,6 +9,9 @@ import io.github.minhaz1217.onlineadvising.Interface.CourseRepository;
 import io.github.minhaz1217.onlineadvising.Interface.StudentRepository;
 import io.github.minhaz1217.onlineadvising.models.Course;
 import io.github.minhaz1217.onlineadvising.models.Student;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.*;
@@ -24,9 +27,11 @@ import org.springframework.web.bind.annotation.*;
 public class StudentController {
        
     protected final StudentRepository studentRepository;
+    private final CourseRepository courseRepository;
 
-    public StudentController(StudentRepository studentRepository){
+    public StudentController(StudentRepository studentRepository, CourseRepository courseRepository){
         this.studentRepository= studentRepository;
+        this.courseRepository = courseRepository;
     }
     //TODO: Remove this /all gate while deplying
     @GetMapping("/all")
@@ -37,6 +42,33 @@ public class StudentController {
 
     @RequestMapping(method = RequestMethod.GET, value = "id/{student}")
     public Student getStudent(@PathVariable String student){
+        //List<Course> fullCourse = courseRepository.findAll();
         return studentRepository.findStudentByStudentCode(student);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "available/{student}")
+    public List<String> findRemainingCourse(@PathVariable String student){
+        List<Course> fullCourse = courseRepository.findAll();
+        List<String> myTaken = Arrays.asList(studentRepository.findStudentByStudentCode(student).getTaken());
+        List<String> available = new ArrayList<>();
+        int flag = 0;
+        for(int i=0;i<fullCourse.size();i++){
+            Course curr = fullCourse.get(i);
+
+            if(myTaken.indexOf(curr.getCourse_code()) == -1){
+                flag = 1;
+                for(int j=0;j<curr.getPrerequisite().size();j++){
+                    if(myTaken.indexOf(curr.getPrerequisite().get(j)) != -1){
+                        flag = 0;
+                        break;
+                    }
+                }
+                if(flag == 1){
+                    available.add(curr.getCourse_code());
+                }
+            }
+        }
+        return available;
+
     }
 }
